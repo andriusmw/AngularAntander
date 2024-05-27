e408173fc18c64bf3d7fc2a4d547d9b3fd3b9d13
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-05-27 14:01:54 +0200

    creating service

---
+++ src/app/services/reading-json-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReadingJsonServiceService {
+
+  private jsonURL = 'assets/json.json';
+
+  constructor(private http: HttpClient) { }
+
+  //La función
+  getData(): Observable<any> {
+    return this.http.get(this.jsonURL)
+  }
+
+}
